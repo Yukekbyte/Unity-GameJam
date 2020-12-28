49572f7c8f39dec6f2bb4d6a3a2c95c64d987a29
Yukekbyte <76691447+Yukekbyte@users.noreply.github.com>
2020-12-28 16:45:38 +0100

    test

--- Dani 2021 GameJam/Assets/test.cs
+++ Dani 2021 GameJam/Assets/test.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public Transform t;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("hey");
+        t = gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
