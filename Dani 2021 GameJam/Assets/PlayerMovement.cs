dbc2bb2079781773745544bff07622a5479551e3
Yukekbyte <76691447+Yukekbyte@users.noreply.github.com>
2021-01-01 19:36:32 +0100

    Player Movement Script
    script geadd voor de playermovement en tijdelijke test grond toegevoegd

--- Dani 2021 GameJam/Assets/PlayerMovement.cs
+++ Dani 2021 GameJam/Assets/PlayerMovement.cs
@@ -1,18 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rb;
+    public float playerSpeed;
+    public float sprintMultiplier;
+    public float jumpForce;
+    public BoxCollider2D boxCollider2D;
+    public LayerMask GroundLayer;
 
-    // Update is called once per frame
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
     void Update()
     {
+        //Player Input
         float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        //Walk
+        Walk(dir);
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Jump(jumpForce);
+        }
+    }
+    
+    //Walk method met een direction (dir) nodig
+    void Walk(Vector2 dir)
+    {
+        rb.velocity = new Vector2(dir.x * playerSpeed * Time.deltaTime, rb.velocity.y);
+    }
+    //Dash method In Progress
+    void Dash()
+    {
+        //In Progress
+    }
+
+    //Jump method met een jumpforce nodig
+    void Jump(float jumpForce)
+    {
+        rb.velocity += new Vector2(0, jumpForce);
+    }
+
+    //Method die checkt ofdat je op de grond staat
+    private bool IsGrounded()
+    {
+        //Stuurt een ray van het midden van de speler naar beneden tot de rand van de boxcollider + margin
+        //returns true wanneer de ray een object met groundlayer raakt, anders false
+        float margin = 0.03f;
+        RaycastHit2D ray = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, margin, GroundLayer);
+        return ray.collider != null;
     }
 }
