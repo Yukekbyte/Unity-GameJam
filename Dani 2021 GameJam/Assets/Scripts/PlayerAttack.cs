eab56c852229643f20390476621a9c538ae9467d
Yukekbyte <76691447+Yukekbyte@users.noreply.github.com>
2021-01-02 14:12:04 +0100

    PlayerAttack and Enemy added

---
+++ Dani 2021 GameJam/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        anim.Play("AttackAnim(tijdelijk)");
+    }
+}
---
+++ Dani 2021 GameJam/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth;
+    private float health;
+
+    void Awake()
+    {
+        health = maxHealth;
+    }
+
+    void TakeDamage(float amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            GameObject.FindObjectOfType<GameManager>().RestartLevel();
+        }
+    }
+}
